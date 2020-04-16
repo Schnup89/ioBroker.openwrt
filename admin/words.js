e9f28809ac83c76b0be664a9404a4fb0d37780e8
Schnup89 <tobias_tsafi@gmx.de>
2020-04-16 18:27:14 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+    "openwrt adapter settings": {
+        "en": "Adapter settings for openwrt",
+        "de": "Adaptereinstellungen für openwrt",
+        "ru": "Настройки адаптера для openwrt",
+        "pt": "Configurações do adaptador para openwrt",
+        "nl": "Adapterinstellingen voor openwrt",
+        "fr": "Paramètres d'adaptateur pour openwrt",
+        "it": "Impostazioni dell'adattatore per openwrt",
+        "es": "Ajustes del adaptador para openwrt",
+        "pl": "Ustawienia adaptera dla openwrt",
+        "zh-cn": "openwrt的适配器设置"
+    },
+    "option1": {
+        "en": "option1",
+        "de": "Option 1",
+        "ru": "Опция 1",
+        "pt": "Opção 1",
+        "nl": "Optie 1",
+        "fr": "Option 1",
+        "it": "opzione 1",
+        "es": "Opción 1",
+        "pl": "opcja 1",
+        "zh-cn": "选项1"
+    },
+    "option2": {
+        "en": "option2",
+        "de": "Option 2",
+        "ru": "option2",
+        "pt": "opção 2",
+        "nl": "Optie 2",
+        "fr": "Option 2",
+        "it": "opzione 2",
+        "es": "opcion 2",
+        "pl": "Opcja 2",
+        "zh-cn": "选项2"
+    }
+};
