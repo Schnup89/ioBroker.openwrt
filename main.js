bff66f588f31a47b19f8fe672ffdc516e7ddde04
Schnup89 <tobias_tsafi@gmx.de>
2020-04-20 23:22:45 +0200

    Bugfix

--- main.js
+++ main.js
@@ -354,7 +354,7 @@ class Openwrt extends utils.Adapter {
                 try {
                     //bug... output \n\t\ seems broken, delete it
                     body = this.replaceAll(body,"\n\t","");
-                    this.setState("sendCommandLastResult",JSON.stringify(body));
+                    this.setState("sendCommandLastResult",body);
                 } catch (e) {
                     this.log.info("##### SendCommand, " + sCMD + " + CatchError: " + e);
                     this.setState("sendCommandLastResult","{ \"error\": \"" + e + "\" }");
