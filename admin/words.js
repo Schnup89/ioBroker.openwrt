3a902119d26c1c48c1937b7c62fd4609d883b3e9
Schnup89 <Tobias_Tsafi@gmx.de>
2021-12-04 20:26:39 +0100

    Update words.js

--- admin/words.js
+++ admin/words.js
@@ -16,55 +16,159 @@ systemDictionary = {
     },
     "lab_general": {
         "en": "General",
-        "de": "Allgemein"
+        "de": "Allgemein",
+        "ru": "Общий",
+        "pt": "Em geral",
+        "nl": "Algemeen",
+        "fr": "Général",
+        "it": "Generale",
+        "es": "General",
+        "pl": "Ogólny",
+        "zh-cn": "一般的"
     },
     "text_general": {
         "en": "OpenWRT RPC API Configuration",
-        "de": "OpenWRT RPC API Konfiguration"
+        "de": "OpenWRT RPC API-Konfiguration",
+        "ru": "Конфигурация OpenWRT RPC API",
+        "pt": "Configuração da API OpenWRT RPC",
+        "nl": "OpenWRT RPC API-configuratie",
+        "fr": "Configuration de l'API OpenWRT RPC",
+        "it": "Configurazione API RPC OpenWRT",
+        "es": "Configuración de la API de OpenWRT RPC",
+        "pl": "Konfiguracja interfejsu API OpenWRT RPC",
+        "zh-cn": "OpenWRT RPC API 配置"
     },
     "lab_url_short": {
         "en": "URL",
-        "de": "URL"
+        "de": "URL",
+        "ru": "URL",
+        "pt": "URL",
+        "nl": "URL",
+        "fr": "URL",
+        "it": "URL",
+        "es": "URL",
+        "pl": "URL",
+        "zh-cn": "网址"
     },
     "lab_url_long": {
         "en": "URL to RPC  |  e.g. http://192.168.1.1/cgi-bin/luci/rpc/",
-        "de": "URL zu RPC-Dienst  |  z.B. http://192.168.1.1/cgi-bin/luci/rpc/"
+        "de": "URL zu RPC | z.B. http://192.168.1.1/cgi-bin/luci/rpc/",
+        "ru": "URL-адрес RPC | например http://192.168.1.1/cgi-bin/luci/rpc/",
+        "pt": "URL para RPC | por exemplo. http://192.168.1.1/cgi-bin/luci/rpc/",
+        "nl": "URL naar RPC | bijv. http://192.168.1.1/cgi-bin/luci/rpc/",
+        "fr": "URL vers RPC | par exemple. http://192.168.1.1/cgi-bin/luci/rpc/",
+        "it": "URL per RPC | per esempio. http://192.168.1.1/cgi-bin/luci/rpc/",
+        "es": "URL a RPC | p.ej. http://192.168.1.1/cgi-bin/luci/rpc/",
+        "pl": "URL do RPC | np. http://192.168.1.1/cgi-bin/luci/rpc/",
+        "zh-cn": "RPC 的 URL |例如http://192.168.1.1/cgi-bin/luci/rpc/"
     },
     "btn_checkURL": {
         "en": "Check URL",
-        "de": "URL prüfen"
+        "de": "URL prüfen",
+        "ru": "Проверить URL",
+        "pt": "Verifique o URL",
+        "nl": "Controleer URL",
+        "fr": "Vérifier l'URL",
+        "it": "Controlla URL",
+        "es": "Verificar URL",
+        "pl": "Sprawdź adres URL",
+        "zh-cn": "检查网址"
     },
     "lab_username_short": {
         "en": "Username",
-        "de": "Benutzername"
+        "de": "Nutzername",
+        "ru": "Имя пользователя",
+        "pt": "Nome do usuário",
+        "nl": "gebruikersnaam",
+        "fr": "Nom d'utilisateur",
+        "it": "Nome utente",
+        "es": "Nombre de usuario",
+        "pl": "Nazwa użytkownika",
+        "zh-cn": "用户名"
     },
     "lab_username_long": {
         "en": "OpenWRT username",
-        "de": "OpenWRT Benutzername"
+        "de": "OpenWRT-Benutzername",
+        "ru": "Имя пользователя OpenWRT",
+        "pt": "Nome de usuário OpenWRT",
+        "nl": "OpenWRT-gebruikersnaam",
+        "fr": "Nom d'utilisateur OpenWRT",
+        "it": "Nome utente OpenWRT",
+        "es": "Nombre de usuario de OpenWRT",
+        "pl": "Nazwa użytkownika OpenWRT",
+        "zh-cn": "OpenWRT 用户名"
     },
     "lab_password_short": {
         "en": "OpenWRT password",
-        "de": "OpenWRT Passwort"
+        "de": "OpenWRT-Passwort",
+        "ru": "Пароль OpenWRT",
+        "pt": "Senha OpenWRT",
+        "nl": "OpenWRT-wachtwoord",
+        "fr": "Mot de passe OpenWRT",
+        "it": "Password OpenWRT",
+        "es": "Contraseña OpenWRT",
+        "pl": "Hasło OpenWRT",
+        "zh-cn": "OpenWRT 密码"
     },
     "lab_password_long": {
         "en": "OpenWRT password",
-        "de": "OpenWRT Passwort"
+        "de": "OpenWRT-Passwort",
+        "ru": "Пароль OpenWRT",
+        "pt": "Senha OpenWRT",
+        "nl": "OpenWRT-wachtwoord",
+        "fr": "Mot de passe OpenWRT",
+        "it": "Password OpenWRT",
+        "es": "Contraseña OpenWRT",
+        "pl": "Hasło OpenWRT",
+        "zh-cn": "OpenWRT 密码"
     },
     "btn_checkAuth": {
         "en": "Check Auth",
-        "de": "Auth prüfen"
+        "de": "Authentifizierung prüfen",
+        "ru": "Проверить аутентификацию",
+        "pt": "Verificar autenticação",
+        "nl": "Controleer Auth",
+        "fr": "Vérifier l'autorisation",
+        "it": "Verifica autenticazione",
+        "es": "Verificar autenticación",
+        "pl": "Sprawdź uwierzytelnianie",
+        "zh-cn": "检查身份验证"
     },
     "lab_refresh_short": {
         "en": "Refresh interval",
-        "de": "Aktualisierungsintervall"
+        "de": "Aktualisierungsintervall",
+        "ru": "Интервал обновления",
+        "pt": "Intervalo de atualização",
+        "nl": "Vernieuwingsinterval",
+        "fr": "Intervalle de rafraîchissement",
+        "it": "Intervallo di aggiornamento",
+        "es": "Intervalo de actualización",
+        "pl": "Częstotliwość odświeżania",
+        "zh-cn": "刷新间隔"
     },
     "lab_refresh_long": {
         "en": "Refresh interval (minutes)",
-        "de": "Aktualisierungsintervall (Minuten)"
+        "de": "Aktualisierungsintervall (Minuten)",
+        "ru": "Интервал обновления (минуты)",
+        "pt": "Intervalo de atualização (minutos)",
+        "nl": "Verversingsinterval (minuten)",
+        "fr": "Intervalle de rafraîchissement (minutes)",
+        "it": "Intervallo di aggiornamento (minuti)",
+        "es": "Intervalo de actualización (minutos)",
+        "pl": "Interwał odświeżania (minuty)",
+        "zh-cn": "刷新间隔（分钟）"
     },
     "lab_settings": {
         "en": "Datatransfer - Commands&DataTypes",
-        "de": "Datenabruf - Kommandos&Datentypen"
+        "de": "Datenübertragung - Befehle und Datentypen",
+        "ru": "Передача данных - Команды и типы данных",
+        "pt": "Datatransfer - Comandos e DataTypes",
+        "nl": "Gegevensoverdracht - Commando's en gegevenstypen",
+        "fr": "Transfert de données - Commandes et types de données",
+        "it": "Trasferimento dati - Comandi e tipi di dati",
+        "es": "Transferencia de datos: comandos y tipos de datos",
+        "pl": "Transfer danych — polecenia i typy danych",
+        "zh-cn": "数据传输 - 命令和数据类型"
     },
     "text_settings": {
         "en": "You probably just need the left table with the Command-List<br>The table on the right should be used if a DataType is determined wrong for a created ioBroker-States [under Tab Objects] (type: Text, Boolean or Number)",
@@ -72,30 +176,86 @@ systemDictionary = {
     },
     "lab_BtnAddCMD": {
         "en": "Add ubus Command",
-        "de": "ubus Kommando hinzufügen"
+        "de": "ubus-Befehl hinzufügen",
+        "ru": "Добавить команду ubus",
+        "pt": "Adicionar comando ubus",
+        "nl": "Ubus-opdracht toevoegen",
+        "fr": "Ajouter une commande ubus",
+        "it": "Aggiungi comando ubus",
+        "es": "Agregar comando ubus",
+        "pl": "Dodaj polecenie ubus",
+        "zh-cn": "添加 ubus 命令"
     },
     "list_commands-cmd": {
         "en": "ubus Command",
-        "de": "ubus Kommando"
+        "de": "ubus-Befehl",
+        "ru": "команда ubus",
+        "pt": "Comando ubus",
+        "nl": "ubus Commando",
+        "fr": "Commande Ubus",
+        "it": "comando ubus",
+        "es": "comando ubus",
+        "pl": "ubus Polecenie",
+        "zh-cn": "ubus 命令"
     },
     "list_commands-del": {
         "en": "Delete",
-        "de": "Löschen"
+        "de": "Löschen",
+        "ru": "Удалить",
+        "pt": "Excluir",
+        "nl": "Verwijderen",
+        "fr": "Effacer",
+        "it": "Elimina",
+        "es": "Borrar",
+        "pl": "Kasować",
+        "zh-cn": "删除"
     },
     "lab_BtnAddType": {
         "en": "Add type definition",
-        "de": "Datentyp hinzufügen"
+        "de": "Typdefinition hinzufügen",
+        "ru": "Добавить определение типа",
+        "pt": "Adicionar definição de tipo",
+        "nl": "Typedefinitie toevoegen",
+        "fr": "Ajouter une définition de type",
+        "it": "Aggiungi la definizione del tipo",
+        "es": "Agregar definición de tipo",
+        "pl": "Dodaj definicję typu",
+        "zh-cn": "添加类型定义"
     },
     "list_types-stateid": {
         "en": "State ID (see Objects)",
-        "de": "State ID (unter Objects)"
+        "de": "State-ID (siehe Objekte)",
+        "ru": "ID состояния (см. Объекты)",
+        "pt": "ID de estado (ver objetos)",
+        "nl": "Status-ID (zie Objecten)",
+        "fr": "ID d'état (voir Objets)",
+        "it": "ID stato (vedi Oggetti)",
+        "es": "ID de estado (ver Objetos)",
+        "pl": "Identyfikator stanu (patrz Obiekty)",
+        "zh-cn": "状态 ID（请参阅对象）"
     },
     "list_types-type": {
         "en": "Datatype",
-        "de": "Datentyp (soll)"
+        "de": "Datentyp (soll)",
+        "ru": "Тип данных",
+        "pt": "Tipo de dados",
+        "nl": "Data type",
+        "fr": "Type de données",
+        "it": "Tipo di dati",
+        "es": "Tipo de datos",
+        "pl": "Typ danych",
+        "zh-cn": "数据类型"
     },
     "list_types-del": {
         "en": "Delete",
-        "de": "Löschen"
+        "de": "Löschen",
+        "ru": "Удалить",
+        "pt": "Excluir",
+        "nl": "Verwijderen",
+        "fr": "Effacer",
+        "it": "Elimina",
+        "es": "Borrar",
+        "pl": "Kasować",
+        "zh-cn": "删除"
     }
 };
